30038b1aa056385ea0b4a42ec26f52355feeddd1
AKP-13 <alexpeirson@hotmail.com>
2020-08-11 15:54:17 +0100

    Add controlled form that fetches to GitHub API and retreives relevant data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from "react";
 import "./styles/App.css";
 class App extends React.Component {
-  state = {};
+  state = { typing: "", username: "", userData:[] };
+  handleChange = (event) => { this.setState({typing: event.target.value}) }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ username: this.state.typing }, () => {
+      let username= this.state.username
+        const url = `https://api.github.com/users/${username}/repos`
+        fetch(url)
+        .then(resp => resp.json())
+          .then(data => {
+          let allData=[]
+          for (const repo of data) {
+            let dataObj = {}
+            dataObj.name=repo.name;
+            dataObj.stargazers=repo.stargazers_count;
+            dataObj.forks=repo.forks_count;
+            allData.push(dataObj)
+          }
+          this.setState({userData: allData})
+        })
+        .catch(err => console.warn('Oh dear...', err))
+    })
+  }
+
   render() {
     return (
       <>
-        <h1>New React App</h1>
-        <h2>Happy Coding</h2>
+        <h1>GitHub Clone</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter GitHub Username </label>
+          <input type="text" value={this.state.typing} onChange={this.handleChange}></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+        <h2>Typing: {this.state.typing}</h2>
+        <h2>Username: {this.state.username}</h2>
+
       </>
     );
   }
